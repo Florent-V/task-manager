9da267e4961f4c400113fcac921daf334ef646e6
Florent-V <vasseurflorent@gmail.com>
2025-01-30 14:52:29 +0100

    fix chunk build

--- client/src/main.js
+++ client/src/main.js
@@ -5,43 +5,43 @@ import App from './App.vue'
 import router from './router'
 import { cropText } from '@/utils/crop.js';
 
-import { OhVueIcon, addIcons } from "oh-vue-icons";
-import { MdClose,
-  MdDelete,
-  MdAdd,
-  MdPhotocamera,
-  MdPhotocameraRound,
-  MdRemoveredeye,
-  MdShareOutlined,
-  MdCheckboxOutlined,
-  MdCheckboxoutlineblank,
-  FaMinus,
-  FaPlus,
-  FaEdit,
-  FaRegularTrashAlt,
-  FaRegularCheckSquare
-} from "oh-vue-icons/icons";
-
-addIcons(MdClose,
-  MdDelete,
-  MdAdd,
-  MdPhotocamera,
-  MdPhotocameraRound,
-  MdRemoveredeye,
-  MdShareOutlined,
-  MdCheckboxOutlined,
-  MdCheckboxoutlineblank,
-  FaMinus,
-  FaPlus,
-  FaEdit,
-  FaRegularTrashAlt,
-  FaRegularCheckSquare
-);
+// import { OhVueIcon, addIcons } from "oh-vue-icons";
+// import { MdClose,
+//   MdDelete,
+//   MdAdd,
+//   MdPhotocamera,
+//   MdPhotocameraRound,
+//   MdRemoveredeye,
+//   MdShareOutlined,
+//   MdCheckboxOutlined,
+//   MdCheckboxoutlineblank,
+//   FaMinus,
+//   FaPlus,
+//   FaEdit,
+//   FaRegularTrashAlt,
+//   FaRegularCheckSquare
+// } from "oh-vue-icons/icons";
+//
+// addIcons(MdClose,
+//   MdDelete,
+//   MdAdd,
+//   MdPhotocamera,
+//   MdPhotocameraRound,
+//   MdRemoveredeye,
+//   MdShareOutlined,
+//   MdCheckboxOutlined,
+//   MdCheckboxoutlineblank,
+//   FaMinus,
+//   FaPlus,
+//   FaEdit,
+//   FaRegularTrashAlt,
+//   FaRegularCheckSquare
+// );
 
 const app = createApp(App)
 
 app.config.globalProperties.$cropText = cropText;
-app.component("v-icon", OhVueIcon);
+//app.component("v-icon", OhVueIcon);
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
