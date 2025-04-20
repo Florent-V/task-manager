7e5e2c2ca9967c8a691e360cbf3553a4c45c4803
Florent-V <vasseurflorent@gmail.com>
2025-04-20 16:17:01 +0200

    feat/kanban : add margin bottom in kanban and fix color in quilljs form new kanban

--- client/src/components/Kanban/KanbanFormComponent.vue
+++ client/src/components/Kanban/KanbanFormComponent.vue
@@ -145,7 +145,7 @@ onMounted(async () => {
       <!-- Description du Kanban -->
       <div id="quill-container" class="mb-4">
         <label for="description" class="block text-gray-700 dark:text-gray-300">Description</label>
-        <div ref="editorContainer" class="border-none w-full bg-white dark:bg-gray-700"></div>
+        <div ref="editorContainer" class="w-full bg-white dark:bg-gray-700"></div>
         <p v-if="errors.description" class="mt-2 text-sm text-red-600 dark:text-red-400">{{ errors.description }}</p>
       </div>
 
@@ -243,7 +243,38 @@ onMounted(async () => {
 
 
 </template>
-<style scoped>
 
+<style scoped>
 </style>
 
+<style>
+.ql-toolbar {
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+}
+
+.ql-container {
+  border-bottom-right-radius: 5px;
+  border-bottom-left-radius: 5px;
+}
+
+.dark .ql-toolbar,
+.dark .ql-container {
+  border-color: none;
+  background-color: #374151;
+}
+
+.dark .ql-toolbar,
+.dark .ql-container {
+  border-color: rgb(75 85 99);
+  background-color: #374151;
+}
+
+.dark .ql-stroke {
+  stroke: #DDD;
+}
+
+.dark .ql-picker {
+  color: #DDD;
+}
+</style>
