6e569bc339db40ee1adbe273609cab51c6e61a8e
Florent-V <vasseurflorent@gmail.com>
2025-06-14 10:15:50 +0200

    déplacement du client dans un dossier service

--- client/src/components/Kanban/KanbanFormComponent.vue
+++ client/src/components/Kanban/KanbanFormComponent.vue
@@ -2,7 +2,7 @@
 import { ref, watch, computed, onMounted } from 'vue';
 import Quill from 'quill';
 
-import { client } from '@/utils/requestMaker.js';
+import { client } from '@/services/requestMaker.js';
 import { hookApi } from "@/utils/requestHook.js";
 import useFormErrors from "@/utils/handleFormErrors.js";
 import logger from "@/utils/logger.js";
--- client/src/views/Kanban/KanbanOverView.vue
+++ client/src/views/Kanban/KanbanOverView.vue
@@ -2,7 +2,7 @@
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 
-import { client } from '@/utils/requestMaker.js';
+import { client } from '@/services/requestMaker.js';
 import { hookApi } from "@/utils/requestHook.js";
 import logger from "@/utils/logger.js";
 import LoaderComponent from "@/components/LoaderComponent.vue";
