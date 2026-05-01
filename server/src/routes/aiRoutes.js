27ffe3180eeb8fb1efcfe890223fb894900c4acf
Florent-V <vasseurflorent@gmail.com>
2026-05-01 11:33:03 +0200

    fix(ai-organize): fix issue in middleware

--- server/src/routes/aiRoutes.js
+++ server/src/routes/aiRoutes.js
@@ -3,6 +3,9 @@ import { authenticateByCookieSession } from '../middleware/authMiddleware.js';
 import { generateToDoList, organizeToDoList } from '../controllers/aiController.js';
 import { authorizeManyToManyRessourceAccess } from '../middleware/ressourceMiddleware.js';
 import { getToDoListById } from '../controllers/toDoListController.js';
+import {
+  setEntity,
+} from '../middleware/toDoListMiddleware.js';
 
 const router = express.Router();
 
@@ -10,6 +13,7 @@ router.post('/generate-todolist', authenticateByCookieSession, generateToDoList)
 router.post(
   '/organize-todolist/:id',
   authenticateByCookieSession,
+  setEntity,
   getToDoListById,
   authorizeManyToManyRessourceAccess,
   organizeToDoList
